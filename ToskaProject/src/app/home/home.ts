6e5ccc8d35c16ea8a07d3f970a49ebf4b691574e
juan-david-restrepo <dayz2006@gmail.com>
2025-09-04 17:18:28 -0500

    agrago mas funciones e vistas

--- ToskaProject/src/app/home/home.ts
+++ ToskaProject/src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Nav } from '../shared/components/nav/nav';
 import { Footer } from '../shared/components/footer/footer';
 
@@ -6,8 +6,39 @@ import { Footer } from '../shared/components/footer/footer';
   selector: 'app-home',
   imports: [Nav, Footer],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrls: ['./home.css'] 
 })
-export class Home {
+export class Home implements OnInit {
+  images: string[] = [
+    'assets/images/redDead.jpg' ,
+    'assets/images/fornite2.jpg',
+    'assets/images/detroit2.jpg',
+    'assets/images/theForest.jpg',
+    
+  ];
+
+  currentIndex = 0;
+  intervalId: any;
+
+  ngOnInit() {
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    this.intervalId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 3000);
+  }
+
+  stopCarousel() {
+    clearInterval(this.intervalId);
+  }
+  prevImage() {
+  this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+}
+
+nextImage() {
+  this.currentIndex = (this.currentIndex + 1) % this.images.length;
+}
 
 }
